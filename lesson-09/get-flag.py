ae2fcc038a2f06458dece73724d8cc843e99d219
David Sutherland <sutherland.dave@gmail.com>
2020-03-01 15:48:39 +0000

    sneaky

---
+++ lesson-09/get-flag.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env ./../bin/python3
+
+def reverse(thing_to_reverse):
+    new_string = ''
+    for character in thing_to_reverse:
+        new_string = new_string + character
+    return new_string
+
+password = input('Please enter the password: ')
+
+if reverse(password) == 'gnihtemos':
+    with open('flag.txt', newline='') as f:
+        print('The flag is: '+f.read(), end='')
+
