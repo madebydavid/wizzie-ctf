c47b8c70a3a0e85c928aa92dfb93ea9c69651481
madebydavid <sutherland.dave@gmail.com>
2020-03-14 00:23:13 +0000

    Random port. Parsing post data

--- lesson-13/get-flag.py
+++ lesson-13/get-flag.py
@@ -6,18 +6,32 @@ import time
 import csv
 
 from http.server import SimpleHTTPRequestHandler
+from urllib.parse import parse_qs
+from random import randint
 
 class BankHTTP(SimpleHTTPRequestHandler):
     def do_POST(self):
-        login_info = self.rfile.read(int(self.headers['Content-Length']))
-        print(login_info)
+        try:
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            login_info = parse_qs(post_data.decode())
+            print(login_info_raw.decode())
+            print(login_info)
+            print(login_info['username'])
+        except:
+            pass
 
-print("Starting bank_server")
-bank_server = socketserver.TCPServer(('', 8000), BankHTTP)
+# Pick a random port number
+port = randint(8000, 9000)
+
+# Start the bank HTTP server
+print('Starting bank_server on port %d' % port)
+bank_server = socketserver.TCPServer(('', port), BankHTTP)
 threading.Thread(target=bank_server.serve_forever).start()
 
+# Open the web browser
 def open_browser():
     time.sleep(3)
-    webbrowser.open_new('http://localhost:8000/index.html')
+    webbrowser.open_new('http://localhost:%d/index.html' % port)
 
 threading.Thread(target=open_browser).start()
