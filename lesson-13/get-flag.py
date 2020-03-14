11ea1834ebaa7ed13dd1554a47f72e806277518d
madebydavid <sutherland.dave@gmail.com>
2020-03-14 14:11:28 +0000

    Web dir and reading users CSV

--- lesson-13/get-flag.py
+++ lesson-13/get-flag.py
@@ -1,37 +1,65 @@
 #!/usr/bin/env ./../bin/python3
+import csv
+import hashlib
+import http.server
+import os
+import random
 import socketserver
-import webbrowser
 import threading
 import time
-import csv
+import urllib.parse
+import webbrowser
 
-from http.server import SimpleHTTPRequestHandler
-from urllib.parse import parse_qs
-from random import randint
+web_dir = os.path.join(os.path.dirname(__file__), 'web')
+os.chdir(web_dir)
 
-class BankHTTP(SimpleHTTPRequestHandler):
+# Load the users and MD5 passwords from the CSV file
+user_passwords = {}
+user_file = open('./users.csv')
+csv_data = csv.reader(user_file)
+next(csv_data) # skip the first row
+for row in csv_data:
+    user_passwords[row[0]] = row[1]
+
+
+class BankHTTP(http.server.SimpleHTTPRequestHandler):
     def do_POST(self):
         try:
+            # Get the username and password values given to us
             content_length = int(self.headers['Content-Length'])
             post_data = self.rfile.read(content_length)
-            login_info = parse_qs(post_data.decode())
-            print(post_data.decode())
-            print(login_info)
-            print(login_info['username'])
-        except:
-            pass
+            login_info = urllib.parse.parse_qs(post_data.decode())
 
-# Pick a random port number
-port = randint(8000, 9000)
+            username = login_info['username'][0]
+            password = login_info['password'][0]
 
-# Start the bank HTTP server
-print('Starting bank_server on port %d' % port)
+            # Check the user exists
+            if username not in user_passwords:
+                raise Exception('Invalid username')
+
+            # Check the password is correct
+            md5_password = hashlib.md5(password.encode()).hexdigest()
+            if user_passwords[username] != md5_password:
+                raise Exception('Invalid password')
+
+            print(user_passwords)
+
+        except Exception as e:
+            self.send_response(301)
+            error = urllib.parse.quote(str(e))
+            self.send_header('Location','/index.html?error=%s' % error)
+            self.end_headers()
+
+
+# Start the bank web server
+port = random.randint(8000, 9000)
+print('Starting bank server on port %d' % port)
 bank_server = socketserver.TCPServer(('', port), BankHTTP)
 threading.Thread(target=bank_server.serve_forever).start()
+
 
 # Open the web browser
 def open_browser():
     time.sleep(3)
     webbrowser.open_new('http://localhost:%d/index.html' % port)
-
 threading.Thread(target=open_browser).start()
